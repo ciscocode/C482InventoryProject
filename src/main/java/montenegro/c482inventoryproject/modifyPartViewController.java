4778ac39ac2b4e625957ab3d719cb22653e760e7
Ciscocode <fmontene92@gmail.com>
2022-12-20 23:01:17 -0800

    fixed a bug in my modify part class in which my code was still attempting to pass an integer/machineId when creating an outsourced part

--- src/main/java/montenegro/c482inventoryproject/modifyPartViewController.java
+++ src/main/java/montenegro/c482inventoryproject/modifyPartViewController.java
@@ -37,7 +37,6 @@ public class modifyPartViewController {
     int min;
     int machineId;
     String companyName;
-    Random randomId = new Random();
     boolean successfulAddition = false;
     int index = 0;
 
@@ -49,6 +48,7 @@ public class modifyPartViewController {
         modifyPartToggleLabel.setText("Company Name");
     }
 
+    //sends the selected parts info into the text fields
     public void sendData(int partIndex, Part part) {
         index = partIndex;
 
@@ -68,50 +68,9 @@ public class modifyPartViewController {
             modifyPartToggleLabel.setText("Company Name");
         }
     }
-    //sends the selected parts info into the text fields
-    public void sendInhousePart(InHouse part) {
-        idTextField.setText(String.valueOf(part.getId()));
-        nameTextField.setText(part.getName());
-        invTextField.setText(String.valueOf(part.getStock()));
-        priceTextField.setText(String.valueOf(part.getPrice()));
-        maxTextField.setText(String.valueOf(part.getMax()));
-        minTextField.setText(String.valueOf(part.getMin()));
-        machineIdTextField.setText(String.valueOf(part.getMachineId()));
-    }
-
-    //send an outsourced parts data
-    public void sendOutsourcedPart(Outsourced part) {
-        idTextField.setText(String.valueOf(part.getId()));
-        nameTextField.setText(part.getName());
-        invTextField.setText(String.valueOf(part.getStock()));
-        priceTextField.setText(String.valueOf(part.getPrice()));
-        maxTextField.setText(String.valueOf(part.getMax()));
-        minTextField.setText(String.valueOf(part.getMin()));
-        machineIdTextField.setText(part.getCompanyName());
-    }
-
-//    public void sendData(Part part) {
-//        if (part instanceof InHouse) {
-//            modifyInhouseRadioButton.setSelected(true);
-//            sendInhousePart((InHouse) part);
-//        } else {
-//            modifyOutsourcedRadioButton.setSelected(true);
-//            sendOutsourcedPart((Outsourced) part);
-//            modifyPartToggleLabel.setText("Company Name");
-//        }
-//    }
 
     public void modifyPart() {
         id = Integer.parseInt(idTextField.getText());
-
-//        //find the inhouse part by searching through the inventory by id
-//        if (modifyInhouseRadioButton.isSelected()) {
-//            InHouse part = (InHouse) lookupPart(id);
-//        } else {
-//            Outsourced part = (Outsourced) lookupPart(id);
-//        }
-
-        //System.out.println(part.getName());
 
         //then once you find the part you can use the setters to modify the part
         name = nameTextField.getText();
@@ -124,6 +83,13 @@ public class modifyPartViewController {
 
         if (modifyOutsourcedRadioButton.isSelected()) {
             companyName = machineIdTextField.getText();
+            if (companyName.isBlank()) {
+                Alert errorMessage = new Alert(Alert.AlertType.WARNING);
+                errorMessage.setTitle("Warning");
+                errorMessage.setContentText("The name is blank!");
+                errorMessage.showAndWait();
+                return;
+            }
         }
 
         //check for invalid data
@@ -146,7 +112,9 @@ public class modifyPartViewController {
             error = "Max";
             max = Integer.parseInt(maxString);
             error = "Machine ID";
-            machineId = Integer.parseInt(machineIdString);
+            if (modifyInhouseRadioButton.isSelected()) {
+                machineId = Integer.parseInt(machineIdString);
+            }
 
             //check to see if min & max values are valid
             if (min > inv) {
@@ -175,131 +143,14 @@ public class modifyPartViewController {
             return;
         }
     }
-    public void modifyInhousePart() {
-        //the id will remain the same
-        id = Integer.parseInt(idTextField.getText());
-
-        //find the inhouse part by searching through the inventory by id
-        InHouse part = (InHouse) lookupPart(id);
-
-        System.out.println(part.getName());
-
-        //then once you find the part you can use the setters to modify the part
-        name = nameTextField.getText();
-        System.out.println(name);
-        String priceString = priceTextField.getText();
-        String invString = invTextField.getText();
-        String minString = minTextField.getText();
-        String maxString = maxTextField.getText();
-        String machineIdString = machineIdTextField.getText();
-
-        String error = "";
-        try {
-            part.setName(name);
-            error = "Price";
-            part.setPrice(Double.parseDouble(priceString));
-            error = "Stock";
-            part.setStock(Integer.parseInt(invString));
-            error = "Min";
-            part.setMin(Integer.parseInt(minString));
-            error = "Max";
-            part.setMax(Integer.parseInt(maxString));
-            error = "Machine ID";
-            part.setMachineId(Integer.parseInt(machineIdString));
-
-            //check to see if min & max values are valid
-            if (min > inv) {
-                System.out.println("Min is higher than available stock!");
-                return;
-            }
-
-            if (inv > max) {
-                System.out.println("Stock must be less than or equal to max!");
-                return;
-            }
-            successfulAddition = true;
-            System.out.println("modifying inhouse part");
-        }
-        catch (NumberFormatException e) {
-            System.out.println(error + " value must be a number!");
-            return;
-        }
-    }
-    public void modifyOutsourcedPart() {
-        //the id will remain the same
-        id = Integer.parseInt(idTextField.getText());
-
-        //find the outsourced part by searching through the inventory by id
-        Outsourced part = (Outsourced) lookupPart(id);
-
-        System.out.println(part.getName());
-
-        //then once you find the part you can use the setters to modify the part
-        name = nameTextField.getText();
-        System.out.println(name);
-        String priceString = priceTextField.getText();
-        String invString = invTextField.getText();
-        String minString = minTextField.getText();
-        String maxString = maxTextField.getText();
-        companyName = machineIdTextField.getText();
-
-        String error = "";
-        try {
-            part.setName(name);
-            error = "Price";
-            part.setPrice(Double.parseDouble(priceString));
-            error = "Stock";
-            part.setStock(Integer.parseInt(invString));
-            error = "Min";
-            part.setMin(Integer.parseInt(minString));
-            error = "Max";
-            part.setMax(Integer.parseInt(maxString));
-            error = "Company Name";
-            part.setCompanyName(companyName);
-
-            //check to see if min & max values are valid
-            if (min > inv) {
-                System.out.println("Min is higher than available stock!");
-                return;
-            }
-
-            if (inv > max) {
-                System.out.println("Stock must be less than or equal to max!");
-                return;
-            }
-            successfulAddition = true;
-            System.out.println("modifying inhouse part");
-        }
-        catch (NumberFormatException e) {
-            System.out.println(error + " value must be a number!");
-            return;
-        }
-    }
 
     public void savePart(ActionEvent actionEvent) throws IOException {
         System.out.println("modifying part");
-
         modifyPart();
         if (successfulAddition == false) {
             return;
         }
 
-//        if (modifyInhouseRadioButton.isSelected()) {
-//            System.out.println("modifying an inhouse part");
-//            //insertInHousePart();
-//            modifyInhousePart();
-//            if (successfulAddition == false) {
-//                return;
-//            }
-//        }
-//
-//        if (modifyOutsourcedRadioButton.isSelected()) {
-//            System.out.println("modifying an outsourced part");
-//            modifyOutsourcedPart();
-//            if (successfulAddition == false) {
-//                return;
-//            }
-//        }
         Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
         Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene((Parent) root, 1303, 494);
